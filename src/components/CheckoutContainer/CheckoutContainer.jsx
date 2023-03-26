ea2f9da397e9639307b41bbe5af39d0d837ba6f8
Tomás Villanueva <tomasino-@hotmail.com>
2023-03-26 01:09:36 -0300

    Changed conditional rendering

--- src/components/CheckoutContainer/CheckoutContainer.jsx
+++ src/components/CheckoutContainer/CheckoutContainer.jsx
@@ -15,32 +15,34 @@ const CheckoutContainer = () => {
     setOrderId(id);
   };
 
-  if (cart.items.length === 0) {
-    return (
-      <div className="mb-6 flex flex-col justify-around px-24">
-        <h1 className="mb-6 pb-2 text-center text-lg">No items in cart!</h1>
-        <Link
-          className="inline-block rounded bg-purple-600 px-7 py-3 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-purple-800 hover:shadow-lg focus:bg-purple-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg"
-          to={appRoutes.home}
-        >
-          Back to home
-        </Link>
-      </div>
-    );
-  }
-
   return (
-    <div className="mx-6 mb-6 flex w-full flex-col justify-around bg-white lg:mx-24">
+    <div className="mx-6 mb-6 flex w-full flex-col justify-around bg-white pb-6 lg:mx-24">
       <h1 className="mb-6 pb-2 text-center text-lg">Checkout</h1>
       {orderId === '' ? (
-        <div className="flex flex-wrap items-center">
-          <div className="w-full lg:w-6/12 xl:w-4/12">
-            <CheckoutForm handleOrderId={handleOrderId} />
-          </div>
-          <div className="lg:w-6/12 xl:w-8/12">
-            <CartList />
-          </div>
-        </div>
+        <>
+          {cart.items.length > 0 ? (
+            <div className="flex flex-wrap items-center">
+              <div className="w-full lg:w-6/12 xl:w-4/12">
+                <CheckoutForm handleOrderId={handleOrderId} />
+              </div>
+              <div className="lg:w-6/12 xl:w-8/12">
+                <CartList />
+              </div>
+            </div>
+          ) : (
+            <div className="mb-6 flex flex-col justify-around px-24">
+              <h1 className="mb-6 pb-2 text-center text-lg">
+                No items in cart!
+              </h1>
+              <Link
+                className="inline-block rounded bg-purple-600 px-7 py-3 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-purple-800 hover:shadow-lg focus:bg-purple-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg"
+                to={appRoutes.home}
+              >
+                Back to home
+              </Link>
+            </div>
+          )}
+        </>
       ) : (
         <CheckoutBrief orderId={orderId} />
       )}
