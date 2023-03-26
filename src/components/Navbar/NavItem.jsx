02484ec1b013deae719a71b93bcc5e0d669d294a
Tomás Villanueva <tomasino-@hotmail.com>
2023-03-26 00:48:04 -0300

    Add navitem selected when route equals path

--- src/components/Navbar/NavItem.jsx
+++ src/components/Navbar/NavItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavItem = ({ children = '', icon, path = '/' }) => {
+  const location = useLocation();
+
+  const onRouteClassName =
+    location.pathname === path
+      ? 'underline underline-offset-8 decoration-4'
+      : '';
+
   return (
     <Link to={path} className="cursor-pointer">
-      <div className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-indigo-600 hover:opacity-75">
+      <div
+        className={`${onRouteClassName} flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-indigo-600 hover:opacity-75`}
+      >
         {icon}
         <span className="ml-2">{children}</span>
       </div>
